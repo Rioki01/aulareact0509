cf97a6952c0cdaa8dd1bf62d099e149719fa1fff
Rioki01 <134452025+Rioki01@users.noreply.github.com>
2023-09-05 20:57:57 -0300

    -commit main
    att react redux

---
+++ aula0509/src/Counter.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const Counter = (props) => {
+    return(
+        <div>
+            <h1>Contador Redux</h1>
+            <p>Contagem: {props.count}</p>
+            <button onClick={props.incremento}>Incrementar</button>
+            <button onClick={props.decremento}>Decrementar</button>
+        </div>
+    );
+};
+
+const mapState = (state) => {
+    return{
+        count: state.count,
+    };
+};
+
+const mapAssociate = (dispatch) => {
+    return{
+        incremento: () => dispatch({type: 'INCREMENTO'}),
+        decremento: () => dispatch({type: 'DECREMENTO'}),
+    };
+};
+
+export default connect(mapState, mapAssociate)(Counter);
